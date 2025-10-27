3a240214459fb660100b1d60f938431a603fb925
René van der Ark <r.van.der.ark@gmail.com>
2025-10-27 16:29:51 +0100

    wip commit.

--- reader/main.ts
+++ reader/main.ts
@@ -1,5 +1,5 @@
-import './css/style.css'
-
+import './css/style.css';
+import './css/highlighting.css';
 import { type FrameClickEvent, type BasicTextSelection } from '@readium/navigator-html-injectables';
 // import Peripherals from './peripherals';
 // import { BasicTextSelection, FrameClickEvent } from "@readium/navigator-html-injectables";
@@ -9,7 +9,7 @@ import { EpubNavigator, type EpubNavigatorListeners } from "@readium/navigator";
 import type { Fetcher, Locator } from "@readium/shared";
 import { HttpFetcher, Manifest, Publication } from "@readium/shared";
 import { Link } from "@readium/shared";
-import { gatherAndPrepareTextNodes } from './helpers/visibleElementHelpers';
+import { gatherAndPrepareTextNodes, isTextNodeVisible, type DocumentTextNodesChunk } from './helpers/visibleElementHelpers';
 import { WebSpeechReadAloudNavigator, type ReadiumSpeechPlaybackEvent } from './readium-speech';
 
 function hideLoadingMessage() {
@@ -18,8 +18,9 @@ function hideLoadingMessage() {
 
 const navigator = new WebSpeechReadAloudNavigator()
 const playButton = document.getElementById("play-readaloud")!;
-
 const VOICE_URI_KEY = "voiceURI";
+let utteranceIndex = -1;
+
 async function initVoices() {
   try {
     const voices = (await navigator.getVoices()).filter(v => v.language.startsWith("nl"))
@@ -63,12 +64,45 @@ initVoices()
 
 const debug = document.getElementById("debug")!;
 const container = document.getElementById("container")!;
+let documentTextNodes : DocumentTextNodesChunk[] = []
+
+function clearHighlights() {
+  document.querySelectorAll(".word-highlight").forEach((el) => el.remove())
+ 
+}
+
+function setWordRects(wordRects : DOMRect[]) {
+  clearHighlights();
+  wordRects.forEach((rect) => {
+    const hlDiv = document.createElement("div");
+    hlDiv.className = "word-highlight";
+    hlDiv.style.top = `${rect.top}px`;
+    hlDiv.style.left = `${rect.left}px`;
+    hlDiv.style.width = `${rect.right - rect.left}px`;
+    hlDiv.style.height = `${rect.bottom - rect.top}px`;
+    container.appendChild(hlDiv);
+  });
+}
 
 function onPlayButtonClicked() {
+  console.log(navigator.getState());
   if (navigator.getState() === "playing") {
       navigator.pause()
+  } else if (navigator.getState() === "paused") {
+    navigator.play()
+  } else if (utteranceIndex > -1) {
+    navigator.jumpTo(utteranceIndex);
+  }
+}
+
+function handlePositionChange(utteranceIndices : number[]) {
+  navigator.stop();
+  if (utteranceIndices.length === 1) {
+    utteranceIndex = utteranceIndices[0];
+  } else if (utteranceIndices.length > 1) {
+    utteranceIndex = utteranceIndices[1];
   } else {
-      navigator.play()
+    utteranceIndex = -1;
   }
 }
 
@@ -77,20 +111,65 @@ function handleWebSpeechNavigatorEvent({ type, detail } : ReadiumSpeechPlaybackE
   switch (navigator.getState()) {
     case "playing":
       playButton.removeAttribute("disabled");
-      playButton.innerHTML = "⏸︎"
+      playButton.innerHTML = "⏸︎";
       break;
     case "loading":
       playButton.setAttribute("disabled", "disabled");
-      playButton.innerHTML = "⏵︎"
+      playButton.innerHTML = "⏵︎";
+      clearHighlights();
       break;
     case "ready":
     case "idle":
+      playButton.removeAttribute("disabled");
+      playButton.innerHTML = "⏵︎";      
+      clearHighlights();
+      break
     case "paused":
     default:
       playButton.removeAttribute("disabled");
-      playButton.innerHTML = "⏵︎"
+      playButton.innerHTML = "⏵︎";
+  }
+  if (type === "boundary" && navigator.getState() === "playing") {
+    const { charIndex, charLength, name } = detail;
+    if (name !== "word") { return; }
+    const utIdx = parseInt(navigator.getCurrentContent()!.id!)
+    let firstTextNodeIndex = -1, lastTextNodeIndex = -1;
+    for (let idx = 0; idx < (documentTextNodes[utIdx]?.rangedTextNodes || []).length; idx++) {
+        const rtn = documentTextNodes[utIdx].rangedTextNodes[idx];
+        if (rtn.parentStartCharIndex <= charIndex) {
+            firstTextNodeIndex = idx;
+            lastTextNodeIndex = idx
+        }
+        if (firstTextNodeIndex > -1 && rtn.parentStartCharIndex + rtn.textNode.textContent!.length <= charIndex + charLength) {
+            lastTextNodeIndex = idx
+        }
+    }
+    if (firstTextNodeIndex > -1) {
+        // const sel = wnd.getSelection();
+        // sel?.removeAllRanges();
+        let newWordRects : DOMRect[] = []
+        for (let rtnIdx = firstTextNodeIndex; rtnIdx <= lastTextNodeIndex; rtnIdx++) {
+            const rtn = documentTextNodes[utIdx].rangedTextNodes[rtnIdx];
+            const chBegin = charIndex - rtn.parentStartCharIndex;
+            const chEnd = charIndex - rtn.parentStartCharIndex + charLength;
+            const rangeBegin = chBegin < 0 ? 0 : chBegin >  (rtn.textNode.textContent || "").length ?  (rtn.textNode.textContent || "").length : chBegin;
+            const rangeEnd = chEnd > (rtn.textNode.textContent || "").length ? (rtn.textNode.textContent || "").length : chEnd
+            const range = new Range()
+            range.setStart(rtn.textNode, rangeBegin);
+            range.setEnd(rtn.textNode, rangeEnd);
+            // sel?.addRange(range);
+            for (let i = 0; i < range.getClientRects().length; i++) {
+                newWordRects.push(range.getClientRects().item(i)!);
+            }
+        }
+        setWordRects(newWordRects)
+    }
+    // if (navigator.getState() === "playing") {
+    //     setUtteranceIndex(utIdx)
+    // }
   }
 }
+
 
 navigator.on("start",handleWebSpeechNavigatorEvent);
 navigator.on("end", handleWebSpeechNavigatorEvent);
@@ -120,15 +199,27 @@ async function init(bookId: string) {
 
       const listeners : EpubNavigatorListeners = {
         frameLoaded: function (wnd: Window): void {
-          const documentTextNodes = gatherAndPrepareTextNodes(wnd);
-            navigator.loadContent(documentTextNodes.map((dtn, idx) => ({
-                id: `${idx}`,
-                text: dtn.utteranceStr
-            })));
+          documentTextNodes = gatherAndPrepareTextNodes(wnd);
+          navigator.loadContent(documentTextNodes.map((dtn, idx) => ({
+              id: `${idx}`,
+              text: dtn.utteranceStr
+          })));
         },
         positionChanged: function (locator: Locator): void {
           hideLoadingMessage();
           console.log("positionChanged locator=", locator)
+          document.querySelectorAll("iframe").forEach((fr) => {
+            if (fr.style.visibility != "hidden") {
+              const navWnd = (fr as HTMLIFrameElement).contentWindow;
+              const utteranceIndices = documentTextNodes.map((dtn, idx) => {
+                  if (dtn.rangedTextNodes.find((rt) => isTextNodeVisible(navWnd!, rt.textNode))) {
+                      return idx;
+                  }
+                  return -1;
+              }).filter((idx) => idx > -1);
+              handlePositionChange(utteranceIndices)
+            }
+          });
         },
         tap: function (e: FrameClickEvent): boolean {
           console.log("tap e=", e )
